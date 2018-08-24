d9a61ab9386f7f755975b3d3f659935cbaf5d76c
Ted Barnett <mail@tedbarnett.com>
2018-08-24 13:12:25 -0700

    Building on Mac, though I can't deploy to the device (Package file /Users/tedbarnett/MagicLeap/TimeWalkMagicLeap/com.timewalk.timewalkmagicleap.package already has a USES declaration! You may not have your own "USES=" statement in the .package file. Please remove it and try again. See the Console for details.)

--- TimeWalkMagicLeap/Assets/MagicLeap/Scripts/MLVersion.cs
+++ TimeWalkMagicLeap/Assets/MagicLeap/Scripts/MLVersion.cs
@@ -81,9 +81,9 @@ namespace UnityEngine.XR.MagicLeap
         /// <summary/>
         public const uint MLSDK_VERSION_REVISION = 0;
         /// <summary/>
-        public const string MLSDK_VERSION_BUILD_ID = "20180714";
+        public const string MLSDK_VERSION_BUILD_ID = "20180720";
 
         /// <summary/>
-        public const string MLSDK_VERSION_NAME = "0.16.0.20180714";
+        public const string MLSDK_VERSION_NAME = "0.16.0.20180720";
     }
 }
